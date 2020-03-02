a87596d29493d53178849f8b6a0c7223fdbefcf7
Tony Martinez <ajosephmartinez@gmail.com>
2020-03-01 16:58:18 -0800

    .

--- src/App.js
+++ src/App.js
@@ -18,7 +18,8 @@ const webAuth = new auth0.WebAuth({
   responseType: "token id_token",
   audience: "https://tonyjmartinez.auth0.com/userinfo",
   scope: "openid email",
-  redirectUri: isDev ? "http://localhost:3000" : "https://www.feedsubscri.be/"
+  // redirectUri: isDev ? "http://localhost:3000" : "https://www.feedsubscri.be/"
+  redirectUri: window.location.origin
 });
 const PRIVATE_ENDPOINT =
   "https://h9gqunf6y7.execute-api.us-west-2.amazonaws.com/dev/api/private";
